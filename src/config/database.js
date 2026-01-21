e9534fc9b28ed940e287cc03ab574543d1cbcf70
prasanthoyasco <prasanthoyasco@gmai.com>
2026-01-21 13:04:15 +0530

    database connection dialectoption added

--- src/config/database.js
+++ src/config/database.js
@@ -8,6 +8,11 @@ module.exports = {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
+    dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false // This is often required for managed providers
+    }
+  },
     logging: console.log,
     pool: {
       max: 10,
